25f0c1fb212a43b0884ff9b45f5da42f5670772a
Levi Winans <levieiko@gmail.com>
2021-01-02 13:35:05 -0800

    add dovenv and firebase

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import firebase from 'firebase';
+import 'firebase/firestore';
+import 'firebase/firebase-storage'
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import * as serviceWorker from './serviceWorker';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCqTKCU8-rk8XaZO9uAiU-TT4MdWmNOeJY",
+  authDomain: "almacards.firebaseapp.com",
+  projectId: "almacards",
+  storageBucket: "almacards.appspot.com",
+  messagingSenderId: "1026487896806",
+  appId: "1:1026487896806:web:3b38fa2d7ef47955354927",
+  measurementId: "G-TZDZ0VMLYN"
+});
+const storage = firebase.storage();
+export { storage, firebase as default }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <Router>
+      <App />
+  </Router>, 
+  document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// serviceWorker.unregister();
