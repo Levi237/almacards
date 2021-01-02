b7803375f551d309fc80882d7567f2ed11c28678
Levi Winans <levieiko@gmail.com>
2021-01-02 13:48:03 -0800

    add styled components and hello world

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-     <iframe src="https://giphy.com/embed/XvZ8PJ4DSqzSM" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/animation-dogs-walt-disney-XvZ8PJ4DSqzSM"></a></p>
-    </div>
+    <AppContainer>
+      <div>
+        <h1>❤️ Hello Alma ❤️</h1>
+        <iframe src="https://giphy.com/embed/XvZ8PJ4DSqzSM" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen />
+      </div>
+    </AppContainer>
   );
 }
 
 export default App;
+
+const AppContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  h1 {
+    text-align: center;
+  }
+  div {
+    display: inline-block;
+  }
+`;
