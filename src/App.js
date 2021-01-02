40b02c26e81ee66aaa6804fd877c29858e6fd33b
Levi Winans <levieiko@gmail.com>
2021-01-02 13:43:09 -0800

    init firebase again

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     hello alma
+     <iframe src="https://giphy.com/embed/XvZ8PJ4DSqzSM" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/animation-dogs-walt-disney-XvZ8PJ4DSqzSM"></a></p>
     </div>
   );
 }
