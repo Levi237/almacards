8071269f7d4ea6b7d0791d90dd1725b228615567
Levi Winans <levieiko@gmail.com>
2021-01-02 13:58:20 -0800

    add spacing

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <div>
         <h1>❤️ Hello Alma ❤️</h1>
         <iframe src="https://giphy.com/embed/XvZ8PJ4DSqzSM" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen />
+        <h1> </h1>
       </div>
     </AppContainer>
   );
