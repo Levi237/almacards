e5509a89a15c4740f22ca6902f77f3aa4045488c
Levi Winans <levieiko@gmail.com>
2021-01-02 21:06:18 -0800

    HomePage add style

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -5,11 +5,25 @@ export default class HomePage extends Component {
     state = {}
     render(){
         return(<>
-            <div>
-              <h1>❤️ Hello Alma ❤️</h1>
-              <iframe src="https://giphy.com/embed/XvZ8PJ4DSqzSM" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen />
-              <h1> </h1>
-            </div>
+            <LocalWrapper>
+                <section>
+                    <h1>❤️ Hello Alma ❤️</h1>
+                    <iframe src="https://giphy.com/embed/XvZ8PJ4DSqzSM" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen />
+                    <h1> </h1>
+                </section>
+            </LocalWrapper>
         </>);
     };
 };
+
+const LocalWrapper = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    section {
+        display: inline-block;
+    }
+`;
