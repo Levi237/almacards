397b98f65326644c08602879cff3af4803e197f9
Levi Winans <levieiko@gmail.com>
2021-01-02 21:06:32 -0800

    clean index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React                        from 'react';
 import ReactDOM                     from 'react-dom';
 import { BrowserRouter as Router }  from 'react-router-dom';
+
 import firebase                     from 'firebase';
 import 'firebase/firestore';
 import 'firebase/firebase-storage'
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 import dotenv from 'dotenv';
 dotenv.config();
 
