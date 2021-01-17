844521bf1840f6d0020918d9d8e7957e5b2e5b1f
Levi Winans <levieiko@gmail.com>
2021-01-17 09:36:04 -0800

    river component

--- src/components/portfolio/hostel/Hostel.js
+++ src/components/portfolio/hostel/Hostel.js
@@ -3,33 +3,16 @@ import styled               from 'styled-components';
 
 import Painting             from './Painting';
 import Floorplan            from './Floorplan';
+import River            from './River';
 ;export default class HostelComponent extends Component {
     render(){
     const { project } = this.props;
         return(
             <>
-           <Painting project={project}/>
-           <Floorplan project={project}/>
-            <GridFour>
-                <img src="./projects/hostel/river.jpg"/>
-                <img src="./projects/hostel/roof-frame-draft.png"/>
-            </GridFour>
+                <Painting project={project}/>
+                <Floorplan project={project}/>
+                <River project={project}/>
             </>
         )
     }
 }
-const GridFour = styled.div`
-    text-align: center;
-    img {
-        &:first-of-type {
-            width: 80%;
-            max-width: 800px;
-            margin: 50px auto;
-        }
-        &:nth-of-type(2) {
-            width: 70%;
-            margin-top: -30vw;
-            opacity: .25;
-        }
-    }
-`;
---
+++ src/components/portfolio/hostel/River.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import styled               from 'styled-components';
+
+;export default class River extends Component {
+    render(){
+    const { project } = this.props;
+        return(
+            <>
+            <GridFour>
+                <img src="./projects/hostel/river.jpg"/>
+                <img src="./projects/hostel/roof-frame-draft.png"/>
+            </GridFour>
+            </>
+        )
+    }
+}
+const GridFour = styled.div`
+    text-align: center;
+    img {
+        &:first-of-type {
+            width: 80%;
+            max-width: 800px;
+            margin: 50px auto;
+        }
+        &:nth-of-type(2) {
+            width: 70%;
+            margin-top: -30vw;
+            opacity: .25;
+        }
+    }
+`;
