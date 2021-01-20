e06e5a830822ff79c35088e4d223b1b11e952de3
Levi Winans <levieiko@gmail.com>
2021-01-20 14:01:58 -0800

    create nav component for modal

---
+++ src/components/portfolio/hostel/VewNav.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class ViewNav extends Component {
+    state = {
+        plans: ['basement', 'first', 'second', 'third', 'fourth']
+    }
+    render(){
+        return(
+
+        );
+    };
+};
